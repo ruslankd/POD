8ae029b90cf9f90b6b27c31daefa8373dc614197
ruslankd <ruslankd@mail.ru>
2021-09-15 22:40:48 +0300

    Lesson 2

--- app/src/main/java/com/example/pod/view/MainActivity.kt
+++ app/src/main/java/com/example/pod/view/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.pod.view
 
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.pod.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val nightModeFlags = resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)
+        if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            val themeId = getSharedPreferences("POD_SP", Context.MODE_PRIVATE)
+                .getInt(SettingsFragment.SP_THEME, R.style.Theme3)
+            setTheme(themeId)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 

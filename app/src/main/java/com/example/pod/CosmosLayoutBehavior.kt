83d4a8d2d55b4a0f311d9177f4f79493e94155b4
ruslankd <ruslankd@mail.ru>
2021-09-23 15:10:56 +0300

    Lesson 4

--- app/src/main/java/com/example/pod/CosmosLayoutBehavior.kt
+++ app/src/main/java/com/example/pod/CosmosLayoutBehavior.kt
@@ -4,18 +4,22 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.ImageView
-import androidx.appcompat.widget.Toolbar
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.appbar.AppBarLayout
 
 
 class CosmosLayoutBehavior(context: Context, attrs: AttributeSet) : CoordinatorLayout.Behavior<ImageView>(context, attrs) {
-    private var mCustomFinalYPosition = 0f
-    private var mCustomStartXPosition = 0f
-    private var mCustomStartToolbarPosition = 0f
-    private var mCustomStartHeight = 0f
-    private var mCustomFinalHeight = 0f
-    private var mAvatarMaxSize = 0f
-    private val mFinalLeftAvatarPadding: Float
+
+    private var mContext: Context = context
+
+    private val mCustomFinalYPosition = 0f
+    private val mCustomStartXPosition = 0f
+    private val mCustomStartToolbarPosition = 0f
+    private val mCustomStartHeight = 0f
+    private val mCustomFinalHeight = 0f
+
+    private val mAvatarMaxSize = 0f
+    private val mFinalLeftAvatarPadding = 0f
     private val mStartPosition = 0f
     private var mStartXPosition = 0
     private var mStartToolbarPosition = 0f
@@ -24,46 +28,51 @@ class CosmosLayoutBehavior(context: Context, attrs: AttributeSet) : CoordinatorL
     private var mStartHeight = 0
     private var mFinalXPosition = 0
     private var mChangeBehaviorPoint = 0f
-    private var mContext: Context = context
-    private fun init() {
-        bindDimensions()
-    }
-
-    private fun bindDimensions() {
-        mAvatarMaxSize = mContext.resources.getDimension(R.dimen.image_width)
-    }
 
     override fun layoutDependsOn(parent: CoordinatorLayout, child: ImageView, dependency: View): Boolean {
-        return dependency is Toolbar
+        return dependency is AppBarLayout
     }
 
     override fun onDependentViewChanged(parent: CoordinatorLayout, child: ImageView, dependency: View): Boolean {
-        maybeInitProperties(child, dependency)
-        val maxScrollDistance = mStartToolbarPosition.toInt()
-        val expandedPercentageFactor = dependency.y / maxScrollDistance
-        if (expandedPercentageFactor < mChangeBehaviorPoint) {
-            val heightFactor = (mChangeBehaviorPoint - expandedPercentageFactor) / mChangeBehaviorPoint
-            val distanceXToSubtract: Float = ((mStartXPosition - mFinalXPosition)
-                    * heightFactor) + child.height / 2
-            val distanceYToSubtract: Float = ((mStartYPosition - mFinalYPosition)
-                    * (1f - expandedPercentageFactor)) + child.height / 2
-            child.x = mStartXPosition - distanceXToSubtract
-            child.y = mStartYPosition - distanceYToSubtract
-            val heightToSubtract = (mStartHeight - mCustomFinalHeight) * heightFactor
-            val lp = child.layoutParams as CoordinatorLayout.LayoutParams
-            lp.width = (mStartHeight - heightToSubtract).toInt()
-            lp.height = (mStartHeight - heightToSubtract).toInt()
-            child.layoutParams = lp
-        } else {
-            val distanceYToSubtract = ((mStartYPosition - mFinalYPosition)
-                    * (1f - expandedPercentageFactor)) + mStartHeight / 2
-            child.x = (mStartXPosition - child.width / 2).toFloat()
-            child.y = mStartYPosition - distanceYToSubtract
-            val lp = child.layoutParams as CoordinatorLayout.LayoutParams
-            lp.width = mStartHeight
-            lp.height = mStartHeight
-            child.layoutParams = lp
-        }
+//        maybeInitProperties(child, dependency)
+//
+//        val maxScrollDistance = mStartToolbarPosition.toInt()
+//        val expandedPercentageFactor: Float = dependency.y / maxScrollDistance
+//
+//        if (true) {
+//            val heightFactor = (mChangeBehaviorPoint - expandedPercentageFactor) / mChangeBehaviorPoint
+//            val distanceXToSubtract = ((mStartXPosition - mFinalXPosition)
+//                    * heightFactor) + child.height / 2
+//            val distanceYToSubtract = ((mStartYPosition - mFinalYPosition)
+//                    * (1f - expandedPercentageFactor)) + child.height / 2
+//            child.x = mStartXPosition - distanceXToSubtract
+//            child.y = mStartYPosition - distanceYToSubtract
+//            val heightToSubtract = (mStartHeight - mCustomFinalHeight) * heightFactor
+//            val lp = child.layoutParams as CoordinatorLayout.LayoutParams
+//            lp.width = (mStartHeight - heightToSubtract).toInt()
+//            lp.height = (mStartHeight - heightToSubtract).toInt()
+//            child.layoutParams = lp
+//        } else {
+//            val distanceYToSubtract = ((mStartYPosition - mFinalYPosition)
+//                    * (1f - expandedPercentageFactor)) + mStartHeight / 2
+//            child.x = (mStartXPosition - child.width / 2).toFloat()
+//            child.y = mStartYPosition - distanceYToSubtract
+//            val lp = child.layoutParams as CoordinatorLayout.LayoutParams
+//            lp.width = mStartHeight
+//            lp.height = mStartHeight
+//            child.layoutParams = lp
+//        }
+
+        val minWidth = mContext.resources.getDimension(R.dimen.image_final_width).toInt()
+        val appBarBottom = (dependency as AppBarLayout).bottom
+        val lp = child.layoutParams as CoordinatorLayout.LayoutParams
+        lp.width =  appBarBottom
+        lp.height =  appBarBottom
+        child.layoutParams = lp
+
+        if (appBarBottom < minWidth) child.visibility = View.INVISIBLE
+        else child.visibility = View.VISIBLE
+
         return true
     }
 
@@ -71,7 +80,7 @@ class CosmosLayoutBehavior(context: Context, attrs: AttributeSet) : CoordinatorL
         if (mStartYPosition == 0) mStartYPosition = dependency.y.toInt()
         if (mFinalYPosition == 0) mFinalYPosition = dependency.height / 2
         if (mStartHeight == 0) mStartHeight = child.height
-        if (mStartXPosition == 0) mStartXPosition = ((child.x + child.width / 2).toInt())
+        if (mStartXPosition == 0) mStartXPosition = (child.x + child.width / 2).toInt()
         if (mFinalXPosition == 0) mFinalXPosition = mContext.resources.getDimensionPixelOffset(R.dimen.abc_action_bar_content_inset_material) + mCustomFinalHeight.toInt() / 2
         if (mStartToolbarPosition == 0f) mStartToolbarPosition = dependency.y
         if (mChangeBehaviorPoint == 0f) {
@@ -79,32 +88,4 @@ class CosmosLayoutBehavior(context: Context, attrs: AttributeSet) : CoordinatorL
         }
     }
 
-    val statusBarHeight: Int
-        get() {
-            var result = 0
-            val resourceId = mContext.resources.getIdentifier("status_bar_height", "dimen", "android")
-            if (resourceId > 0) {
-                result = mContext.resources.getDimensionPixelSize(resourceId)
-            }
-            return result
-        }
-
-    companion object {
-        private const val MIN_AVATAR_PERCENTAGE_SIZE = 0.3f
-        private const val EXTRA_FINAL_AVATAR_PADDING = 80
-        private const val TAG = "behavior"
-    }
-
-    init {
-        val a = mContext.obtainStyledAttributes(attrs, R.styleable.CosmosLayoutBehavior)
-        mCustomFinalYPosition = a.getDimension(R.styleable.CosmosLayoutBehavior_finalYPosition, 0f)
-        mCustomStartXPosition = a.getDimension(R.styleable.CosmosLayoutBehavior_startXPosition, 0f)
-        mCustomStartToolbarPosition = a.getDimension(R.styleable.CosmosLayoutBehavior_startToolbarPosition, 0f)
-        mCustomStartHeight = a.getDimension(R.styleable.CosmosLayoutBehavior_startHeight, 0f)
-        mCustomFinalHeight = a.getDimension(R.styleable.CosmosLayoutBehavior_finalHeight, 0f)
-        a.recycle()
-        init()
-        mFinalLeftAvatarPadding = mContext.resources.getDimension(
-                R.dimen.spacing_normal)
-    }
 }
--- app/src/main/java/com/example/pod/view/CosmosFragment.kt
+++ app/src/main/java/com/example/pod/view/CosmosFragment.kt
@@ -11,7 +11,7 @@ import com.google.android.material.appbar.AppBarLayout
 import kotlin.math.abs
 
 
-class CosmosFragment : Fragment(), AppBarLayout.OnOffsetChangedListener {
+class CosmosFragment : Fragment() {
     private var _binding: FragmentCosmosBinding? = null
     private val binding: FragmentCosmosBinding
         get() {
@@ -21,13 +21,6 @@ class CosmosFragment : Fragment(), AppBarLayout.OnOffsetChangedListener {
     companion object {
         fun newInstance() = CosmosFragment()
     }
-
-    private val PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.9f
-    private val PERCENTAGE_TO_HIDE_TITLE_DETAILS = 0.3f
-    private val ALPHA_ANIMATIONS_DURATION = 200
-
-    private var mIsTheTitleVisible = false
-    private var mIsTheTitleContainerVisible = true
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -40,52 +33,5 @@ class CosmosFragment : Fragment(), AppBarLayout.OnOffsetChangedListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.mainAppbar.addOnOffsetChangedListener(this@CosmosFragment)
-
-        startAlphaAnimation(binding.mainTextviewTitle, 0, View.INVISIBLE);
-    }
-
-    override fun onOffsetChanged(appBarLayout: AppBarLayout?, offset: Int) {
-        val maxScroll = appBarLayout!!.totalScrollRange
-        val percentage = abs(offset).toFloat() / maxScroll.toFloat()
-
-        handleAlphaOnTitle(percentage)
-        handleToolbarTitleVisibility(percentage)
-    }
-
-    private fun handleToolbarTitleVisibility(percentage: Float) {
-        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
-            if (!mIsTheTitleVisible) {
-                startAlphaAnimation(binding.mainTextviewTitle, ALPHA_ANIMATIONS_DURATION.toLong(), View.VISIBLE)
-                mIsTheTitleVisible = true
-            }
-        } else {
-            if (mIsTheTitleVisible) {
-                startAlphaAnimation(binding.mainTextviewTitle, ALPHA_ANIMATIONS_DURATION.toLong(), View.INVISIBLE)
-                mIsTheTitleVisible = false
-            }
-        }
-    }
-
-    private fun handleAlphaOnTitle(percentage: Float) {
-        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
-            if (mIsTheTitleContainerVisible) {
-                startAlphaAnimation(binding.mainLinearlayoutTitle, ALPHA_ANIMATIONS_DURATION.toLong(), View.INVISIBLE)
-                mIsTheTitleContainerVisible = false
-            }
-        } else {
-            if (!mIsTheTitleContainerVisible) {
-                startAlphaAnimation(binding.mainLinearlayoutTitle, ALPHA_ANIMATIONS_DURATION.toLong(), View.VISIBLE)
-                mIsTheTitleContainerVisible = true
-            }
-        }
-    }
-
-    private fun startAlphaAnimation(v: View, duration: Long, visibility: Int) {
-        val alphaAnimation = if (visibility == View.VISIBLE) AlphaAnimation(0f, 1f) else AlphaAnimation(1f, 0f)
-        alphaAnimation.duration = duration
-        alphaAnimation.fillAfter = true
-        v.startAnimation(alphaAnimation)
     }
 }
